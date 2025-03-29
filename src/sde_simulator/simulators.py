607a0207a46e49dc840fb51563e7581c1ea4fb7e
Far3000-YT <far3000yt@gmail.com>
2025-03-29 19:19:57 +0100

    day 3, soon ipynb

--- src/sde_simulator/simulators.py
+++ src/sde_simulator/simulators.py
@@ -55,3 +55,36 @@ def simulate_gbm_em_vectorized(s0, mu, sigma, T, dt, num_paths):
         S[i+1, :] = S_prev + (sigma * S_prev * Z_step * sqrt_dt) + (mu * S_prev * dt) #same formula as above but we vectorized it
 
     return t, S
+
+
+def simulate_gbm_milstein_vectorized(s0, mu, sigma, T, dt, num_paths, Z=None):
+    #same variables as before !
+    n_steps = int(T / dt)
+    t = np.linspace(0, T, n_steps + 1)
+
+    S = np.zeros((n_steps + 1, num_paths))
+    S[0, :] = s0
+
+    #we generate a random z if not provided
+    if Z is None: Z = np.random.normal(0, 1, size=(n_steps, num_paths))
+    elif Z.shape != (n_steps, num_paths): raise ValueError("Provided Z array has incorrect shape")
+
+    sqrt_dt = np.sqrt(dt)
+
+    #loop through time steps
+    for i in range(n_steps):
+        S_prev = S[i, :] #current step
+        Z_step = Z[i, :] #random shock with Z on every i
+        
+        dW = Z_step * sqrt_dt
+
+        #Milstein formula here (in diff parts, basically = actual price + euler term + milstein correction)
+        euler_term = (mu * S_prev * dt) + (sigma * S_prev * dW)
+        
+        #correction term when using Z : 0.5 * sigma^2 * S_prev * dt * (Z_step**2 - 1)
+        milstein_correction = 0.5 * (sigma**2) * S_prev * dt * (Z_step**2 - 1)
+        
+        #then price update
+        S[i+1, :] = S_prev + euler_term + milstein_correction
+
+    return t, S
