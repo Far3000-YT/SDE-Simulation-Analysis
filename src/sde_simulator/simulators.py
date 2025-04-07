2988714cf328f02d49a1af37b7a16332f5ca3216
Far3000-YT <far3000yt@gmail.com>
2025-04-07 16:53:50 +0200

    day 7

--- src/sde_simulator/simulators.py
+++ src/sde_simulator/simulators.py
@@ -90,3 +90,47 @@ def simulate_gbm_milstein_vectorized(s0, mu, sigma, T, dt, num_paths, Z = None):
         S[i+1, :] = S_prev + euler_term + milstein_correction
 
     return t, S
+
+
+def simulate_ou_em_vectorized(x0, theta, kappa, sigma, T, dt, num_paths, Z=None):
+    """
+    It simulates multiple paths of the Ornstein-Uhlenbeck process using the Euler-Maruyama process in a vectorized manner
+
+    x0 : initial value
+    theta : speed of mean reversion (theta > 0).
+    kappa : long-term mean level
+    sigma : volatility coefficient
+    T : time horizon (years)
+    dt : time step size
+    num_paths : number of paths
+    Z : pre-generated standard normal random numbers
+    """
+
+    n_steps = int(T / dt)
+    t = np.linspace(0, T, n_steps + 1)
+
+    X = np.zeros((n_steps + 1, num_paths))
+    X[0, :] = x0
+
+    if Z is None:
+        Z = np.random.normal(0, 1, size=(n_steps, num_paths))
+    elif Z.shape != (n_steps, num_paths):
+        raise ValueError("Provided Z array has incorrect shape")
+
+    sqrt_dt = np.sqrt(dt)
+
+    #loop through time steps (vectorized across paths)
+    for i in range(n_steps):
+        X_prev = X[i, :] #current
+        Z_step = Z[i, :] #random shock for all prices
+
+        #dW calc for the step
+        dW = Z_step * sqrt_dt
+
+        #Euler-Maruyama formula for OU:
+        #dX = theta*(kappa - X)*dt + sigma*dW
+        X[i+1, :] = X_prev + theta * (kappa - X_prev) * dt + sigma * dW
+
+        #and unlike GBM, OU process can become negative !
+
+    return t, X
